f5205a51e39c76c0d462fd39987618f1f49e18eb
studioshane <studioshane@gmail.com>
2019-03-15 12:54:16 +0000

    youtube button not working yet

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,19 @@
 // Code DigitalClicker Component Here
+
+import React, { Component } from "react";
+
+export default class DigitalClicker extends Component {
+  state = {
+    timesClicked: 0
+  };
+
+  increaseClick = () => {
+    this.setState({ timesClicked: this.state.timesClicked + 1 });
+  };
+
+  render() {
+    return (
+      <button onClick={this.increaseClick}>{this.state.timesClicked}</button>
+    );
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,32 @@
 // Code YouTubeDebugger Component Here
+import React from "react";
+
+export default class YouTubeDebugger extends React.Component {
+  state = {
+    js: {
+      errors: [],
+      user: null,
+      settings: { bitrate: 8, video: { resolution: "1080p" } }
+    }
+  };
+
+
+
+  changeRes = () => {
+        this.setState(state.js.settings.video.resolution: "720p") 
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.changeRes} className="resolution">
+          {this.state.js.settings.video.resolution}
+        </button>
+
+        <button className="bitrate">{this.state.js.settings.bitrate}</button>
+      </div>
+    );
+  }
+}
